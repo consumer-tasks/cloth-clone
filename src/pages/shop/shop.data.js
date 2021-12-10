5bcd516c9e8a2f07750d4384a9c5d10519a18bc5
ojs-hard <farlight216@gmail.com>
2021-12-10 00:19:28 -0800

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,41 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0k/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwqmp/brown-cowboy.png',
+                price: 35
+            },
+            {
+                id: 4,
+                name: 'Grey Brim',
+                imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Green Beanie',
+                imageUrl: 'https://i.ibb.co/YTjw3vF/green-beanie.png',
+                price: 18
+            }
+        ]
+    }
+]
+
+export default SHOP_DATA;
